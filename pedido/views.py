87a1b4f5503a95cdfbb5de9e90059884682c99e3
Matheus Machado <matheusmachado2005@hotmail.com>
2024-07-16 17:56:52 -0300

    Adicionado URLs e Imagens.

--- pedido/views.py
+++ pedido/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views import View
+from django.http import HttpResponse
 
-# Create your views here.
+class Pagar(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Pagar')
+
+class FecharPedido(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('FecharPedido')
+
+class Detalhe(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Detalhe')
