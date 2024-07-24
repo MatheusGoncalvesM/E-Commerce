3553f1163089849274ac1d8e69b2b237298f8cd3
Matheus Machado <matheusmachado2005@hotmail.com>
2024-07-24 20:59:01 -0300

    Primeiro Commit no perfil

--- perfil/views.py
+++ perfil/views.py
@@ -2,21 +2,35 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views import View
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
 
-class Criar(View):
+
+from . import models
+from . import forms
+
+class BasePerfil(View):
+    template_name = 'perfil/criar.html'
+
+    def setup(self, *args, **kwargs):
+        super().setup(*args, **kwargs)
+
+        self.contexto = {
+            'userform': forms.userForm(
+                data=self.request.POST or None
+            ),
+            'perfilform': forms.PerfilForm(
+                data=self.request.POST or None
+            )
+            }
+
+        self.renderizar = render(self.request, self.template_name, self.contexto)
+
     def get(self, *args, **kwargs):
-        return HttpResponse('Criar')
+        return self.renderizar
     
+class Criar(BasePerfil):
+    pass
+
 class Atualizar(View):
     def get(self, *args, **kwargs):
         return HttpResponse('Atualizar')
-
-class Login(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Login')
-    
-class Logout(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Logout')
-    
-
